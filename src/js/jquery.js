207d7bf0f28447e6d3e8fcda7500dd909b2b6844
testing777 <paraslearnings@gmail.com>
2019-02-23 01:22:08 -0500

    update code

--- src/js/jquery.js
+++ src/js/jquery.js
@@ -1,17 +1,2 @@
 global.jQuery = require('jquery');
-mustache = require('mustache');
-
-
-jQuery(document).ready(function($){
-    alert('hello')
-    var jqxhr = $.getJSON('data.json', function(){
-
-    }).done(function(data){
-        var template = $('#template').html();
-        var showTemplate = mustache.render(template, data);
-        ('#gallery').html(showTemplate);
-    });
-    
-});
-
 var bootstrap = require('bootstrap');
